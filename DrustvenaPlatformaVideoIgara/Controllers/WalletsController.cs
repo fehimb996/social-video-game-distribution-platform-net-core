c2f4ce4809ed56ec875535ac81deef2bb71edda2
fehimb996 <kingring2009@live.com>
2024-07-12 19:56:07 +0200

    added wallets controller

---
+++ DrustvenaPlatformaVideoIgara/Controllers/WalletsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DrustvenaPlatformaVideoIgara.Models;
+
+namespace DrustvenaPlatformaVideoIgara.Controllers
+{
+    public class WalletsController : Controller
+    {
+        private readonly SteamContext _context;
+
+        public WalletsController(SteamContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Wallets
+        public async Task<IActionResult> Index()
+        {
+            var steamContext = _context.Wallets.Include(w => w.User);
+            return View(await steamContext.ToListAsync());
+        }
+
+        // GET: Wallets/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var wallet = await _context.Wallets
+                .Include(w => w.User)
+                .FirstOrDefaultAsync(m => m.WalletId == id);
+            if (wallet == null)
+            {
+                return NotFound();
+            }
+
+            return View(wallet);
+        }
+
+        // GET: Wallets/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId");
+            return View();
+        }
+
+        // POST: Wallets/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("WalletId,UserId,Balance")] Wallet wallet)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(wallet);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", wallet.UserId);
+            return View(wallet);
+        }
+
+        // GET: Wallets/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var wallet = await _context.Wallets.FindAsync(id);
+            if (wallet == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", wallet.UserId);
+            return View(wallet);
+        }
+
+        // POST: Wallets/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("WalletId,UserId,Balance")] Wallet wallet)
+        {
+            if (id != wallet.WalletId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(wallet);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WalletExists(wallet.WalletId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", wallet.UserId);
+            return View(wallet);
+        }
+
+        // GET: Wallets/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var wallet = await _context.Wallets
+                .Include(w => w.User)
+                .FirstOrDefaultAsync(m => m.WalletId == id);
+            if (wallet == null)
+            {
+                return NotFound();
+            }
+
+            return View(wallet);
+        }
+
+        // POST: Wallets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var wallet = await _context.Wallets.FindAsync(id);
+            if (wallet != null)
+            {
+                _context.Wallets.Remove(wallet);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool WalletExists(int id)
+        {
+            return _context.Wallets.Any(e => e.WalletId == id);
+        }
+    }
+}
