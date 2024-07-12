13eca6a0e8c9983fb940a3515e6a9f9c9b5623ef
fehimb996 <kingring2009@live.com>
2024-07-12 17:42:16 +0200

    Products controller added

--- DrustvenaPlatformaVideoIgara/Program.cs
+++ DrustvenaPlatformaVideoIgara/Program.cs
@@ -1,7 +1,13 @@
+using DrustvenaPlatformaVideoIgara.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<SteamContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
