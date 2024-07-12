628ed31f21037a381eb867bb41a4e9bee72434b4
fehimb996 <kingring2009@live.com>
2024-07-12 19:51:01 +0200

    added messages controller

---
+++ DrustvenaPlatformaVideoIgara/Controllers/MessagesController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DrustvenaPlatformaVideoIgara.Models;
+
+namespace DrustvenaPlatformaVideoIgara.Controllers
+{
+    public class MessagesController : Controller
+    {
+        private readonly SteamContext _context;
+
+        public MessagesController(SteamContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Messages
+        public async Task<IActionResult> Index()
+        {
+            var steamContext = _context.Messages.Include(m => m.UserId1Navigation).Include(m => m.UserId2Navigation);
+            return View(await steamContext.ToListAsync());
+        }
+
+        // GET: Messages/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var message = await _context.Messages
+                .Include(m => m.UserId1Navigation)
+                .Include(m => m.UserId2Navigation)
+                .FirstOrDefaultAsync(m => m.MessageId == id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+
+            return View(message);
+        }
+
+        // GET: Messages/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId1"] = new SelectList(_context.Users, "UserId", "UserId");
+            ViewData["UserId2"] = new SelectList(_context.Users, "UserId", "UserId");
+            return View();
+        }
+
+        // POST: Messages/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MessageId,UserId1,UserId2,MessageContent,Timestamp")] Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(message);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId1"] = new SelectList(_context.Users, "UserId", "UserId", message.UserId1);
+            ViewData["UserId2"] = new SelectList(_context.Users, "UserId", "UserId", message.UserId2);
+            return View(message);
+        }
+
+        // GET: Messages/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var message = await _context.Messages.FindAsync(id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId1"] = new SelectList(_context.Users, "UserId", "UserId", message.UserId1);
+            ViewData["UserId2"] = new SelectList(_context.Users, "UserId", "UserId", message.UserId2);
+            return View(message);
+        }
+
+        // POST: Messages/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MessageId,UserId1,UserId2,MessageContent,Timestamp")] Message message)
+        {
+            if (id != message.MessageId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(message);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MessageExists(message.MessageId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId1"] = new SelectList(_context.Users, "UserId", "UserId", message.UserId1);
+            ViewData["UserId2"] = new SelectList(_context.Users, "UserId", "UserId", message.UserId2);
+            return View(message);
+        }
+
+        // GET: Messages/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var message = await _context.Messages
+                .Include(m => m.UserId1Navigation)
+                .Include(m => m.UserId2Navigation)
+                .FirstOrDefaultAsync(m => m.MessageId == id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+
+            return View(message);
+        }
+
+        // POST: Messages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var message = await _context.Messages.FindAsync(id);
+            if (message != null)
+            {
+                _context.Messages.Remove(message);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MessageExists(int id)
+        {
+            return _context.Messages.Any(e => e.MessageId == id);
+        }
+    }
+}
