2228bab6365ddf149ef2cd3d6c6184f6b67b807f
fehimb996 <kingring2009@live.com>
2024-08-08 20:33:44 +0200

    user profile update

--- DrustvenaPlatformaVideoIgara/Controllers/GenresController.cs
+++ DrustvenaPlatformaVideoIgara/Controllers/GenresController.cs
@@ -18,13 +18,11 @@ namespace DrustvenaPlatformaVideoIgara.Controllers
             _context = context;
         }
 
-        // GET: Genres
         public async Task<IActionResult> Index()
         {
             return View(await _context.Genres.ToListAsync());
         }
 
-        // GET: Genres/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -42,15 +40,11 @@ namespace DrustvenaPlatformaVideoIgara.Controllers
             return View(genre);
         }
 
-        // GET: Genres/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Genres/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("GenreId,GenreName")] Genre genre)
@@ -64,7 +58,6 @@ namespace DrustvenaPlatformaVideoIgara.Controllers
             return View(genre);
         }
 
-        // GET: Genres/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -80,9 +73,6 @@ namespace DrustvenaPlatformaVideoIgara.Controllers
             return View(genre);
         }
 
-        // POST: Genres/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("GenreId,GenreName")] Genre genre)
@@ -115,7 +105,6 @@ namespace DrustvenaPlatformaVideoIgara.Controllers
             return View(genre);
         }
 
-        // GET: Genres/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -133,7 +122,6 @@ namespace DrustvenaPlatformaVideoIgara.Controllers
             return View(genre);
         }
 
-        // POST: Genres/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
