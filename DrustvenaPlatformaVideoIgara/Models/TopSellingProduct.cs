15187557140e53277c580bf457a1908f9de071ab
fehimb996 <kingring2009@live.com>
2024-08-27 19:11:44 +0200

    Improvements on Products index page

--- DrustvenaPlatformaVideoIgara/Models/TopSellingProduct.cs
+++ DrustvenaPlatformaVideoIgara/Models/TopSellingProduct.cs
@@ -5,5 +5,6 @@
         public int ProductId { get; set; }
         public string ProductName { get; set; }
         public decimal Price { get; set; }
+        public string ImagePath { get; set; }
     }
 }
